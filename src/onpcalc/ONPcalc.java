e62969e0c65478f567bb4edcd05dab55db09ba01
Pawel Krynski <krynskip@ee.pw.edu.pl>
2015-10-13 17:21:01 +0200

    W pełni działający kalkulator z poprawionymi zabezpieczeniami przed wprowadzeniem niewłaściwych wyrażeń.

--- src/onpcalc/ONPcalc.java
+++ src/onpcalc/ONPcalc.java
@@ -7,47 +7,53 @@ import stack.Stack;
  * @author Paweł
  */
 public class ONPcalc {
-
+    
     public static void count(String[] input) throws Stack.EmptyStackException {
-
+        
         Stack stos = new Stack();
-
+        
         for (String expr : input) {
             System.out.println("Argument: " + expr);
-
+            
             Double number = null;
-
+            
             try {
                 number = Double.parseDouble(expr);
             } catch (Exception e) {
-                System.out.println("Wyrażenie nie może być przetworzone: " + expr);
+                if (!expr.equals("+") && !expr.equals("-") && !expr.equals("x") && !expr.equals(":")) {
+                    System.out.println("Wyrażenie nie może być przetworzone: " + expr);
+                    return;
+                }
             }
-
+            
             if (number != null) {
                 stos.put(number);
             } else if (expr.equals("+")) {
                 double second = stos.get();
                 double first = stos.get();
                 stos.put(first + second);
-
+                
             } else if (expr.equals("-")) {
                 double second = stos.get();
                 double first = stos.get();
                 stos.put(first - second);
-
+                
             } else if (expr.equals("x")) {
                 double second = stos.get();
                 double first = stos.get();
                 stos.put(first * second);
-
+                
             } else if (expr.equals(":")) {
                 double second = stos.get();
                 double first = stos.get();
                 stos.put(first / second);
             }
         }
+        
+        System.out.println("Wynik: " + stos.get());
+        
     }
-
+    
     public static void main(String[] args) {
         
         try {
@@ -55,7 +61,7 @@ public class ONPcalc {
         } catch (Stack.EmptyStackException ex) {
             System.out.println("Zła składnia: za mało liczb / za dużo operacji");
         }
-
+        
     }
-
+    
 }
