67a6a3df902d72a0482f3033cc092b52ce8d574e
Pawel Krynski <krynskip@ee.pw.edu.pl>
2015-10-16 19:50:24 +0200

    Rozpoznawanie nieprawidłowych wyrażeń (za dużo liczb / za mało operatorów)

--- src/onpcalc/ONPcalc.java
+++ src/onpcalc/ONPcalc.java
@@ -7,16 +7,16 @@ import stack.Stack;
  * @author Paweł
  */
 public class ONPcalc {
-    
+
     public static void count(String[] input) throws Stack.EmptyStackException {
-        
+
         Stack stos = new Stack();
-        
+
         for (String expr : input) {
-            System.out.println("Argument: " + expr);
-            
+            //System.out.println("Argument: " + expr);
+
             Double number = null;
-            
+
             try {
                 number = Double.parseDouble(expr);
             } catch (Exception e) {
@@ -25,43 +25,49 @@ public class ONPcalc {
                     return;
                 }
             }
-            
+
             if (number != null) {
                 stos.put(number);
             } else if (expr.equals("+")) {
                 double second = stos.get();
                 double first = stos.get();
                 stos.put(first + second);
-                
+
             } else if (expr.equals("-")) {
                 double second = stos.get();
                 double first = stos.get();
                 stos.put(first - second);
-                
+
             } else if (expr.equals("x")) {
                 double second = stos.get();
                 double first = stos.get();
                 stos.put(first * second);
-                
+
             } else if (expr.equals(":")) {
                 double second = stos.get();
                 double first = stos.get();
                 stos.put(first / second);
             }
         }
-        
-        System.out.println("Wynik: " + stos.get());
-        
+
+        double result = stos.get();
+
+        if (stos.isEmpty()) {
+            System.out.println("Wynik: " + result);
+        } else {
+            System.out.println("Nieprawidłowe wyrażenie: za dużo liczb / za mało operatorów");
+        }
+
     }
-    
+
     public static void main(String[] args) {
-        
+
         try {
             count(args);
         } catch (Stack.EmptyStackException ex) {
             System.out.println("Zła składnia: za mało liczb / za dużo operacji");
         }
-        
+
     }
-    
+
 }
